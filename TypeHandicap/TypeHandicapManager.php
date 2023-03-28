61fd2625a98b03a8d69f96a28f9177c4121c68f7
AHOUZI HASNAE <92023794+hasnae21@users.noreply.github.com>
2023-03-28 14:23:30 +0000

    save
    save

--- TypeHandicap/TypeHandicapManager.php
+++ TypeHandicap/TypeHandicapManager.php
@@ -15,6 +15,18 @@
 
             <?php include("../content-header.php") ?>
 
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0">Gestion de type d'handicap</h1>
+                        </div><!-- /.col -->
+                       
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
 
             <!-- Main content -->
             <section class="content">
