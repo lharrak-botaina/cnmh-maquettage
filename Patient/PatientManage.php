4465ec139389acd32680436935d5a7ebbb4e4f59
Zayd-Elkaissi <zayd.elkaissi.solicode@gmail.com>
2023-03-28 11:45:31 +0000

    Patient

---
+++ Patient/PatientManage.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("../head.php") ?>
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+<?php include("../menu.php") ?>
+<?php include("../navbar.php") ?>
+
+<div class="content-wrapper">
+
+
+<?php include("../content-header.php") ?>
+
+<section class="content">
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+             <h2>Index</h2>
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</section>
+
+
+</div>
+
+<?php include("../footer.php") ?>
+</div>
+<?php include("../scripts.php") ?>
+</body>
+</html>
