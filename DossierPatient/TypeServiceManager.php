6cef230541d53146b3d9c109138b5b3ab9831d49
lharrak-botaina <lharrak.botaina@gmail.com>
2023-03-29 14:59:54 +0000

    [ADD]DossierPatient

---
+++ DossierPatient/TypeServiceManager.php
@@ -0,0 +1,144 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include("../head.php") ?>
+    <link rel="stylesheet" href="https://unpkg.com/bs-stepper/dist/css/bs-stepper.min.css">
+<script src="https://unpkg.com/bs-stepper/dist/js/bs-stepper.min.js"></script>
+
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <?php include("../menu.php") ?>
+        <?php include("../navbar.php") ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">  
+
+            <?php include("../content-header.php") ?>
+
+ <!-- Content Header (Page header) -->
+ <div class="content-header">
+      <div class="container-fluid">
+        <div class="row mb-2">
+          <div class="col-sm-6">
+            <h1 class="m-0">Gestion des services</h1>
+          </div><!-- /.col -->
+
+        </div><!-- /.row -->
+      </div><!-- /.container-fluid -->
+    </div>
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <!-- Default box -->
+                
+                    <div class="row">
+          <div class="col-md-12">
+            <div class="card card-default">
+              <div class="card-header">
+                <h3 class="card-title">bs-stepper</h3>
+              </div>
+              <div class="card-body p-0">
+                <div class="bs-stepper" id="stepper1">
+                  <div class="bs-stepper-header" role="tablist">
+                    <!-- your steps here -->
+                    <div class="step" data-target="#logins-part">
+                      <button type="button" class="step-trigger" role="tab" aria-controls="logins-part" id="logins-part-trigger">
+                        <span class="bs-stepper-circle">1</span>
+                        <span class="bs-stepper-label">Logins</span>
+                      </button>
+                    </div>
+                    <div class="line"></div>
+                    <div class="step" data-target="#information-part">
+                      <button type="button" class="step-trigger" role="tab" aria-controls="information-part" id="information-part-trigger">
+                        <span class="bs-stepper-circle">2</span>
+                        <span class="bs-stepper-label">Various information</span>
+                      </button>
+                    </div>
+                  </div>
+                  <div class="bs-stepper-content">
+                    <!-- your steps content here -->
+                    <div id="logins-part" class="content" role="tabpanel" aria-labelledby="logins-part-trigger">
+                      <div class="form-group">
+                        <label for="exampleInputEmail1">Email address</label>
+                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
+                      </div>
+                      <div class="form-group">
+                        <label for="exampleInputPassword1">Password</label>
+                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+                      </div>
+                      <button class="btn btn-primary"   onclick="stepper1.next()">Next</button>
+                    </div>
+                    <div id="information-part" class="content" role="tabpanel" aria-labelledby="information-part-trigger">
+                      <div class="form-group">
+                        <label for="exampleInputFile">File input</label> 
+                        <div class="input-group">
+                          <div class="custom-file">
+                            <input type="file" class="custom-file-input" id="exampleInputFile">
+                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
+                          </div>
+                          <div class="input-group-append">
+                            <span class="input-group-text">Upload</span>
+                          </div>
+                        </div>
+                        
+                      </div>
+                      <button class="btn btn-primary" onclick="stepper1.previous()">Previous</button>
+                      <button type="submit" class="btn btn-primary">Submit</button>
+                    </div>
+                  </div>
+                </div>
+              </div>
+              <!-- /.card-body -->
+              <div class="card-footer">
+                Visit <a href="https://github.com/Johann-S/bs-stepper/#how-to-use-it">bs-stepper documentation</a> for more examples and information about the plugin.
+              </div>
+            </div>
+            <!-- /.card -->
+          </div>
+        </div>
+
+                    <!-- /.card -->
+                    <div class="card-header row" style="display: flow-root;">
+                        <div style="display: flex;  justify-content: space-between;">
+
+                            <ul class="pagination pagination my-0">
+                                <li class="page-item"><a class="page-link" href="#">«</a></li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">»</a></li>
+                            </ul>
+                            <div class="">
+                                
+                                <button type="button" class="btn btn-default swalDefaultQuestion">
+                                    <i class="fas fa-download"></i> Export
+                                </button>
+                                <button type="button" class="btn btn-default swalDefaultQuestion">
+                                    <i class="fas fa-file-import"></i> Import
+                                </button>
+                            </div>
+                        </div>
+                    </div><!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
+        </div>
+
+        <?php include("../footer.php") ?>
+    </div>
+    <?php include("../scripts.php") ?>
+    <script src="dist/js/bs-stepper.js"></script>
+    <script>
+      var stepper1Node = document.querySelector('#stepper1')
+      var stepper1 = new Stepper(document.querySelector('#stepper1'))
+
+    //   stepper1Node.addEventListener('show.bs-stepper', function (event) {
+    //     console.warn('show.bs-stepper', event)
+    //   })
+    
+    </script>
+</body>
+
+</html>
