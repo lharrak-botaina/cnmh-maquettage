5cf7d95867c13f2a9c585506bf0a4946f9288fff
EL.MLIKI.HICHAM <92031524+El-Mliki-Hicham@users.noreply.github.com>
2023-03-29 02:11:26 -0700

    maquette entity

--- Entity/EntityManager.php
+++ Entity/EntityManager.php
@@ -1,36 +1,136 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <?php include("../head.php") ?>
 </head>
+
 <body class="hold-transition sidebar-mini">
-<div class="wrapper">
-<?php include("../menu.php") ?>
-<?php include("../navbar.php") ?>
+    <div class="wrapper">
+        <?php include("../menu.php") ?>
+        <?php include("../navbar.php") ?>
 
-<div class="content-wrapper">
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+
+            <?php include("../content-header.php") ?>
 
 
-<?php include("../content-header.php") ?>
+            <!-- Main content -->
+            <section class="content">
+                <div class="container-fluid">
+                    <!-- Default box -->
+                    <div class="card">
+                        <div class="card-header">
+                            <h3 class="card-title">
+                            Entity Manager
+                            </h3>
 
-<section class="content">
-    <div class="container-fluid">
-    <div class="row">
-        <div class="col-12">
-             <h2>Index</h2>
+                            <div class="col-sm-12 d-flex justify-content-between p-3">
+                                <div class="d-flex justify-content-between">
+                                    <a href="./TypeHandicapForm.php" class="btn btn-primary"> Add Entity </a>
+                                   
+                                </div>
+                                <!-- SEARCH FORM -->
+                                <form class="form-inline ml-3">
+                                    <div class="input-group input-group-sm">
+                                   
+                            <input type="search" class="form-control form-control-lg" placeholder="Type your keywords here">
+                            <div class="input-group-append">
+                                <button type="submit" class="btn btn-lg btn-default">
+                                    <i class="fa fa-search"></i>
+                                </button>
+                        
+                        </div>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                        <div class="card-body p-0">
+                            <table class="table table-striped projects">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 1%">
+                                            #
+                                        </th>
+                                        <th style="width: 20%">
+                                            Name Type Handicape
+                                        </th>
+
+                                        <th style="width: 20%">
+                                            Actions
+                                        </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                        <td>
+                                            #
+                                        </td>
+                                        <td>
+                                            AHOUZI
+                                        </td>
+
+                                        <td class="project-actions text-right">
+                                            <a class="btn btn-primary btn-sm" href="#">
+                                                <i class="fas fa-folder">
+                                                </i>
+                                                View
+                                            </a>
+                                            <a class="btn btn-info btn-sm" href="#">
+                                                <i class="fas fa-pencil-alt">
+                                                </i>
+                                                Edit
+                                            </a>
+                                            <a class="btn btn-danger btn-sm" href="#">
+                                                <i class="fas fa-trash">
+                                                </i>
+                                                Delete
+                                            </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                    <div class="card-header row" >
+                            <div class="float-right col-md-6" >
+                                <ul class="pagination pagination-sm" >
+                                    <li class="page-item"><a class="page-link" href="#">«</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                    <li class="page-item"><a class="page-link" href="#">»</a></li>
+                                </ul>
+                            </div>
+                            <div class="float-left col-md-6 d-flex justify-content-end" style="align-items: center; ">
+                            <!-- <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">
+                                <i class="fas fa-download"></i> export-pdf</button>
+                                <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;">  <i class="fas fa-download"></i> export_excel</button>
+                                <button type="button" class="btn btn-primary float-right" style="margin-right: 5px;"> <i class="fas fa-download"></i> import_excel</button> -->
+                               
+                                <button type="button" class="btn btn-default swalDefaultQuestion">
+                                <i class="fas fa-download"></i> export_pdf
+                              </button>
+                                <button type="button" class="btn btn-default swalDefaultQuestion">
+                                <i class="fas fa-download"></i> export_excel
+                              </button>
+                                <button type="button" class="btn btn-default swalDefaultQuestion">
+                                <i class="fas fa-file-import"></i> import_excel
+                              </button>
+                        
+                    </div>
+                </div><!-- /.container-fluid -->
+            </section>
+            <!-- /.content -->
         </div>
-        <!-- /.col -->
+
+        <?php include("../footer.php") ?>
     </div>
-    <!-- /.row -->
-    </div>
-    <!-- /.container-fluid -->
-</section>
-
-
-</div>
-
-<?php include("../footer.php") ?>
-</div>
-<?php include("../scripts.php") ?>
+    <?php include("../scripts.php") ?>
 </body>
+
 </html>
