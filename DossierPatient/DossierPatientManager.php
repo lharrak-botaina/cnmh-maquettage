23a56e17818bcab99045b6fa97bc5d3a2905a434
lharrak-botaina <lharrak.botaina@gmail.com>
2023-03-30 01:49:34 +0000

    [ADD] patient & parent form to dossierPatient

--- DossierPatient/DossierPatientManager.php
+++ DossierPatient/DossierPatientManager.php
@@ -47,54 +47,27 @@
                     <div class="step" data-target="#logins-part">
                       <button type="button" class="step-trigger" role="tab" aria-controls="logins-part" id="logins-part-trigger">
                         <span class="bs-stepper-circle">1</span>
-                        <span class="bs-stepper-label">Logins</span>
+                        <span class="bs-stepper-label">Les parents</span>
                       </button>
                     </div>
                     <div class="line"></div>
                     <div class="step" data-target="#information-part">
                       <button type="button" class="step-trigger" role="tab" aria-controls="information-part" id="information-part-trigger">
                         <span class="bs-stepper-circle">2</span>
-                        <span class="bs-stepper-label">Various information</span>
+                        <span class="bs-stepper-label">Patient</span>
                       </button>
                     </div>
+                    
                   </div>
                   <div class="bs-stepper-content">
                     <!-- your steps content here -->
-                    <div id="logins-part" class="content" role="tabpanel" aria-labelledby="logins-part-trigger">
-                      <div class="form-group">
-                        <label for="exampleInputEmail1">Email address</label>
-                        <input type="email" class="form-control" id="exampleInputEmail1" placeholder="Enter email">
-                      </div>
-                      <div class="form-group">
-                        <label for="exampleInputPassword1">Password</label>
-                        <input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
-                      </div>
-                      <button class="btn btn-primary"   onclick="stepper1.next()">Next</button>
-                    </div>
-                    <div id="information-part" class="content" role="tabpanel" aria-labelledby="information-part-trigger">
-                      <div class="form-group">
-                        <label for="exampleInputFile">File input</label> 
-                        <div class="input-group">
-                          <div class="custom-file">
-                            <input type="file" class="custom-file-input" id="exampleInputFile">
-                            <label class="custom-file-label" for="exampleInputFile">Choose file</label>
-                          </div>
-                          <div class="input-group-append">
-                            <span class="input-group-text">Upload</span>
-                          </div>
-                        </div>
-                        
-                      </div>
-                      <button class="btn btn-primary" onclick="stepper1.previous()">Previous</button>
-                      <button type="submit" class="btn btn-primary">Submit</button>
-                    </div>
+                    <?php include("./ParentForm.php") ?>
+                    <?php include("./PatientForm.php") ?>
                   </div>
                 </div>
               </div>
               <!-- /.card-body -->
-              <div class="card-footer">
-                Visit <a href="https://github.com/Johann-S/bs-stepper/#how-to-use-it">bs-stepper documentation</a> for more examples and information about the plugin.
-              </div>
+             
             </div>
             <!-- /.card -->
           </div>
@@ -137,7 +110,7 @@
     //   stepper1Node.addEventListener('show.bs-stepper', function (event) {
     //     console.warn('show.bs-stepper', event)
     //   })
-    
+  
     </script>
 </body>
 
