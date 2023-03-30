f1c717e56b3292e14a18e7d38d3dea5fe0e29252
Zayd-Elkaissi <zayd.elkaissi.solicode@gmail.com>
2023-03-30 11:51:46 +0000

    Reclamation

--- head.php
+++ head.php
@@ -11,5 +11,7 @@
   <link rel="stylesheet" href="/assets/plugins/datatables-bs4/css/dataTables.bootstrap4.min.css">
   <link rel="stylesheet" href="/assets/plugins/datatables-responsive/css/responsive.bootstrap4.min.css">
   <link rel="stylesheet" href="/assets/plugins/datatables-buttons/css/buttons.bootstrap4.min.css">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bs-stepper/dist/js/bs-stepper.min.js">
+  <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bs-stepper/dist/css/bs-stepper.min.css">
   <!-- Theme style -->
   <link rel="stylesheet" href="/assets/dist/css/adminlte.min.css">
