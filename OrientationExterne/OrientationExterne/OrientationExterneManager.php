e8420cac9937b374dc06c2417e8d948ecf2470c5
aichabenayad <114480806+aichabenayad@users.noreply.github.com>
2023-03-30 14:51:36 +0000

    save

---
+++ OrientationExterne/OrientationExterne/OrientationExterneManager.php
@@ -0,0 +1,182 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <?php include("../../head.php") ?>
+</head>
+
+<body class="hold-transition sidebar-mini">
+    <div class="wrapper">
+        <?php include("../../menu.php") ?>
+        <?php include("../../navbar.php") ?>
+
+        <!-- Content Wrapper. Contains page content -->
+        <div class="content-wrapper">
+
+            <?php include("../../content-header.php") ?>
+            <div class="bs-stepper">
+  <div class="bs-stepper-header" role="tablist">
+    <!-- your steps here -->
+    <div class="step" data-target="#logins-part">
+      <button type="button" class="step-trigger" role="tab" aria-controls="logins-part" id="logins-part-trigger">
+        <span class="bs-stepper-circle">1</span>
+        <span class="bs-stepper-label">Parent</span>
+      </button>
+    </div>
+    <div class="line"></div>
+    <div class="step" data-target="#information-part">
+      <button type="button" class="step-trigger" role="tab" aria-controls="information-part" id="information-part-trigger">
+        <span class="bs-stepper-circle">2</span>
+        <span class="bs-stepper-label">Patient</span>
+      </button>
+    </div>
+    <div class="line"></div>
+    <div class="step" data-target="#information-part">
+      <button type="button" class="step-trigger" role="tab" aria-controls="information-part" id="information-part-trigger">
+        <span class="bs-stepper-circle">3</span>
+        <span class="bs-stepper-label">Accompagnement</span>
+      </button>
+    </div>
+  </div>
+  <div class="bs-stepper-content">
+    <!-- your steps content here -->
+    <div id="logins-part" class="content" role="tabpanel" aria-labelledby="logins-part-trigger"></div>
+    <div id="information-part" class="content" role="tabpanel" aria-labelledby="information-part-trigger"></div>
+  </div>
+</div>
+            <!-- Content Header (Page header) -->
+            <div class="content-header">
+                <div class="container-fluid">
+                    <div class="row mb-2">
+                        <div class="col-sm-6">
+                            <h1 class="m-0"> Parent</h1>
+                        </div><!-- /.col -->
+                       
+                    </div><!-- /.row -->
+                </div><!-- /.container-fluid -->
+            </div>
+            <!-- /.content-header -->
+
+            <!-- Main content -->
+            <!-- <form action="" id="wizard"> -->
+                <section class="content">
+                <div class="container-fluid">
+                    <!-- Default box -->
+                    <div class="card">
+                        <div class="card-header">
+                            <!-- <h3 class="card-title">
+                            Pa
+                            tients
+                            </h3> -->
+
+                            <div class="col-sm-12 d-flex justify-content-between p-3">
+                                <div class="d-flex justify-content-between">
+                                    <div class="d-flex justify-content-between">
+                                        <a href="../Parent.php" class="btn btn-primary"> <i class="fa fa-plus"></i> </a>
+                                    </div>
+                                    <!-- <select class="btn btn-secondary dropdown-toggle ml-2" name="filter" id="filter">
+                                        <option value="">Nom</option>
+                                        <option value="">Hamza</option>
+                                        <option value="">Kamal</option>
+                                    </select> -->
+                                </div>
+                                <!-- SEARCH FORM -->
+                                <form class="form-inline ml-3">
+                                    <div class="input-group input-group-sm">
+                                        <input type="search" class="form-control form-control-lg" placeholder="Tapez vos mots-clés ici">
+                                        <div class="input-group-append">
+                                            <button type="submit" class="btn btn-lg btn-default">
+                                                <i class="fa fa-search"></i>
+                                            </button>
+
+                                        </div>
+                                    </div>
+                                </form>
+
+                            </div>
+                        </div>
+                        <div class="card-body p-0">
+                            <table class="table table-striped projects">
+                                <thead>
+                                    <tr>
+                                        <th style="width: 10%"> Nom </th>
+                                        <th style="width: 10%"> Prenom </th>
+                                        <th style="width: 10%"> Telephon </th>
+                                        <th style="width: 10%" class="">  </th>
+                                    </tr>
+                                </thead>
+                                <tbody>
+                                    <tr>
+                                   <td> AHOUZI </td>
+                                        <td> Hanan </td>
+                                        <td> 0645729821 </td>
+                                        <td class="project-actions text-right">
+                                            <a class="btn btn-primary btn-sm" href="../PatientManager.php"> <i class="fas fa-check"> </i> Suivre </a>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                       <td> Mnari </td>
+                                        <td> Karim </td>
+                                        <td> 0645729821 </td>
+                                        <td class="project-actions text-right">
+                                            <a class="btn btn-primary btn-sm" href="../PatientManager.php"> <i class="fas fa-check"> </i> Suivre </a>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                       <td> alami </td>
+                                        <td> Salma </td>
+                                        <td> 0645729821 </td>
+                                        <td class="project-actions text-right">
+                                            <a class="btn btn-primary btn-sm" href="../PatientManager.php"> <i class="fas fa-check"> </i> Suivre </a>
+                                        </td>
+                                    </tr>
+
+                                    <tr>
+                                       <td> Nwino </td>
+                                        <td> Nabila </td>
+                                        <td> 0645729821 </td>
+                                        <td class="project-actions text-right">
+                                            <a class="btn btn-primary btn-sm" href="../PatientManager.php"> <i class="fas fa-check"> </i> Suivre </a>
+                                        </td>
+                                    </tr>
+                                </tbody>
+                            </table>
+                        </div>
+                        <!-- /.card-body -->
+                    </div>
+                    <!-- /.card -->
+                    <div class="card-header row">
+                        <div class="float-right col-md-6">
+                            <ul class="pagination pagination my-0">
+                                <li class="page-item"><a class="page-link" href="#">«</a></li>
+                                <li class="page-item"><a class="page-link" href="#">1</a></li>
+                                <li class="page-item"><a class="page-link" href="#">2</a></li>
+                                <li class="page-item"><a class="page-link" href="#">3</a></li>
+                                <li class="page-item"><a class="page-link" href="#">»</a></li>
+                            </ul>
+                        </div>
+                        <div class="float-left col-md-6 d-flex justify-content-end" style="align-items: center; ">
+                            <!-- <button type="button" class="btn btn-default swalDefaultQuestion">
+                                <i class="fas fa-download"></i> export_pdf
+                            </button> -->
+                            <button type="button" class="btn btn-default mr-2 swalDefaultQuestion">
+                                <i class="fas fa-download"></i> export
+                            </button>
+                            <button type="button" class="btn btn-default swalDefaultQuestion">
+                                <i class="fas fa-file-import"></i> import
+                            </button>
+                        </div><!-- /.container-fluid -->
+            </section>
+            </form>
+        
+            <!-- /.content -->
+        </div>
+
+        <?php include("../../footer.php") ?>
+    </div>
+    <?php include("../../scripts.php") ?>
+</body>
+
+</html>
---
+++ OrientationExterne/Parent.php
@@ -0,0 +1,139 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <?php include("../head.php") ?>
+</head>
+<body class="hold-transition sidebar-mini">
+<div class="wrapper">
+<?php include("../menu.php") ?>
+<?php include("../navbar.php") ?>
+
+<div class="content-wrapper">
+    <?php include("../content-header.php") ?>
+
+<section class="content">
+    <div class="container-fluid">
+    <div class="row">
+        <div class="col-12">
+             <h2>Formulaire</h2>
+             <div class="col-md-12">
+            <div class="card card-primary">
+              <div class="card-header">
+                <h3 class="card-title">Ajouter un Parents</h3>
+              </div>
+              <div class="card-body">
+                <!-- Date -->
+                <div class="d-flex">
+                <div class="form-group col-6">
+                  <label> Nom </label>
+                    <div class="input-group date" id="reservationdate" data-target-input="nearest">
+                        <input type="text" class="form-control datetimepicker-input" />
+                        <!-- <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
+                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        </div> -->
+                    </div>
+                </div>
+                <div class="form-group col-6">
+                  <label> Prénom </label>
+                    <div class="input-group date" id="reservationdate" data-target-input="nearest">
+                        <input type="text" class="form-control datetimepicker-input" />
+                        <!-- <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
+                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        </div> -->
+                    </div>
+                </div>
+                </div>
+                <div class="d-flex">
+                <div class="form-group col-6 ">
+                  <label> Sexe </label>
+                    <div class="input-group date" id="reservationdate" data-target-input="nearest">
+                        <!-- <input type="text" class="form-control datetimepicker-input" /> -->
+                        <select type="text"class="form-control datetimepicker-input" class="btn btn-white dropdown-toggle ml-2" name="Sexe" id="filter">
+                                        <option value="">homme</option>
+                                        <option value="">femme</option>
+                                    </select>
+                        <!-- <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
+                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        </div> -->
+                    </div>
+                    </div>
+                
+               
+
+                <div class="form-group col-6">
+                  <label> CIN </label>
+                    <div class="input-group date" id="reservationdate" data-target-input="nearest">
+                        <input type="text" class="form-control datetimepicker-input" />
+                        <!-- <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
+                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        </div> -->
+                    </div>
+                </div>
+                </div>
+                 
+                <div class="d-flex">
+                <div class="form-group col-6">
+                  <label> Téléphone </label>
+                    <div class="input-group date" id="reservationdate" data-target-input="nearest">
+                        <input type="text" class="form-control datetimepicker-input" />
+                        <!-- <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
+                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        </div> -->
+                    </div>
+                    </div>
+                
+                
+                <div class="form-group col-6">
+                  <label> Adresse </label>
+                    <div class="input-group date" id="reservationdate" data-target-input="nearest">
+                        <input type="text" class="form-control datetimepicker-input" />
+                        <!-- <div class="input-group-append" data-target="#reservationdate" data-toggle="datetimepicker">
+                            <div class="input-group-text"><i class="fa fa-calendar"></i></div>
+                        </div> -->
+                    </div>
+                </div>
+                </div>
+                <!-- Date and time -->
+                
+                <div class="d-flex justify-content-between">
+                  <a href="PatientManager.php" class="btn btn-primary">Suivre</a>
+                  <a href="../OrientationExterne/OrientationExterne/OrientationExterneManager.php" class="btn btn-outline-secondary">Annuler</a>
+                </div>
+                <!-- /.form group -->
+                <!-- Date range -->
+
+                <!-- /.form group -->
+
+                <!-- Date and time range -->
+
+                <!-- /.form group -->
+
+                <!-- Date and time range -->
+
+                <!-- /.form group -->
+              </div>
+
+
+              <!-- /.card-body -->
+            </div>
+            <!-- /.card -->
+
+            <!-- iCheck -->
+
+            <!-- /.card -->
+          </div>
+        </div>
+        <!-- /.col -->
+    </div>
+    <!-- /.row -->
+    </div>
+    <!-- /.container-fluid -->
+</section>
+
+</div>
+
+<?php include("../footer.php") ?>
+</div>
+<?php include("../scripts.php") ?>
+</body>
+</html>
