f25a33b4b72d6801d059b1bde09940a084c766bb
imane-aj <ajroudi.im@gmail.com>
2023-04-03 09:36:19 +0000

    menu

--- Menu/InfMedGenerale.php
+++ Menu/InfMedGenerale.php
@@ -2,7 +2,7 @@
       <ul class="nav nav-pills nav-sidebar flex-column" data-widget="treeview" role="menu" data-accordion="false">
 
         <li class="nav-item">
-          <a href="/Patient/PatientManager.php" class="nav-link">
+          <a href="/Patient/PatientManager.php" class="nav-link active">
           <i class="fa-regular fa-calendar-check"></i>
             <p class="pl-2">
             Rendez-vous
