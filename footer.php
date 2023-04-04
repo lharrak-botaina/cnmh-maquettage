97650c3fad34d952153363a11dd5011cea83788c
Merradou Abderrahmane <abderahmane.merradou@gmail.com>
2023-04-04 10:52:42 +0000

    remove link CNMH

--- footer.php
+++ footer.php
@@ -3,7 +3,7 @@
     <div class="float-right d-none d-sm-block">
       <b>Version</b> 1.0
     </div>
-    <strong>Copyright &copy; 2023-2024 <a href="https://adminlte.io">CNMH</a>.</strong> All rights reserved.
+    <strong>Copyright &copy; 2023-2024 CNMH.</strong> All rights reserved.
   </footer>
 
   <!-- Control Sidebar -->
